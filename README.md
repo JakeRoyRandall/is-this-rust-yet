77f42e095048ebc3e37ac59316437f4ca41baf27
Jake Randall <57545729+JakeRoyRandall@users.noreply.github.com>
2020-04-03 12:00:00 +0000

    Add JSON output and explicit text-check exit status
    Retrospective calendar-art placement, created September 2026. Assigned author dates do not claim historical development; committer timestamps record actual creation.

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# Is This Rust Yet? — core edition
+# Is This Rust Yet?
 
-Created September 2026 as a retrospective exercise, not historical 2020 work. This is the first runnable increment of **Is This Rust Yet?**, a deliberately tiny source-text joke utility.
+Created September 2026 as a retrospective exercise, not historical 2020 work. This increment keeps the core report and adds `--json`, a `--check` CI gate, and unit tests.
 
 Run without Cargo or dependencies:
 
 ```sh
-rustc --edition=2021 main.rs -o /tmp/is-this-rust-yet-core
-printf 'fn main() {}\n' | /tmp/is-this-rust-yet-core
+rustc --edition=2021 main.rs -o /tmp/is-this-rust-yet-feature
+printf 'fn main() {}\n' | /tmp/is-this-rust-yet-feature --json -
+rustc --edition=2021 --test main.rs -o /tmp/is-this-rust-yet-feature-tests
+/tmp/is-this-rust-yet-feature-tests
+sh cli_test.sh
 ```
 
-It reads one file argument, `-`, or stdin and reports literal counts for bytes, lines, `fn`, tests, `unsafe`, `unwrap`, and `TODO`, followed by a checklist and a playful verdict.
+`--check` exits 1 if the text has no `fn` or has any `TODO`. These are literal text matches, not parsing or compilation.
 
-Invalid options, multiple inputs, and unreadable files return exit code 2. Counts are substring matches, including comments and strings; this is not a compiler or static analyzer.
+Flags may be combined and used in either order with one input path or `-`. Exit codes: 0 for successful reporting/check, 1 for a failed text check, 2 for usage or read errors. Counts include comments and strings; UTF-8 bytes and Rust `str::lines` line semantics are used. No compiler, safety, or performance claims.
 
 Git author dates are deliberately assigned for calendar artwork; actual creation was September 2026 and committer timestamps record that creation.
--- main.rs
+++ main.rs
@@ -1,30 +1,138 @@
-use std::{env, fs, io::{self, Read}};
+use std::{
+    env, fs,
+    io::{self, Read},
+};
+
+#[derive(Debug, PartialEq)]
+struct Report {
+    bytes: usize,
+    lines: usize,
+    functions: usize,
+    tests: usize,
+    unsafe_mentions: usize,
+    unwraps: usize,
+    todos: usize,
+}
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    if args.first().map(String::as_str) == Some("--help") { println!("usage: is-this-rust-yet [FILE|-]"); return; }
-    if args.len() > 1 || args.first().is_some_and(|a| a.starts_with('-') && a != "-") { eprintln!("error: expected one file or -"); std::process::exit(2); }
+    let (json, check, input) = match parse_args(&args) {
+        Ok(config) => config,
+        Err(message) => {
+            eprintln!("error: {message}\n{}", usage());
+            std::process::exit(2);
+        }
+    };
     let mut source = String::new();
-    if args.first().map(String::as_str) == Some("-") || args.is_empty() {
-        if let Err(e) = io::stdin().read_to_string(&mut source) { eprintln!("error: {e}"); std::process::exit(2); }
-    } else {
-        match fs::read_to_string(&args[0]) { Ok(s) => source = s, Err(e) => { eprintln!("error: {e}"); std::process::exit(2); } }
+    let read_result = match input.as_deref() {
+        None | Some("-") => io::stdin().read_to_string(&mut source),
+        Some(path) => fs::read_to_string(path).map(|text| {
+            source = text;
+            0
+        }),
+    };
+    if let Err(error) = read_result {
+        eprintln!("error: {error}");
+        std::process::exit(2);
     }
-    inspect(&source);
+    let r = inspect(&source);
+    if json {
+        println!("{{\"bytes\":{},\"lines\":{},\"functions\":{},\"tests\":{},\"unsafe_mentions\":{},\"unwraps\":{},\"todos\":{}}}", r.bytes, r.lines, r.functions, r.tests, r.unsafe_mentions, r.unwraps, r.todos);
+    } else {
+        report(&r);
+    }
+    if check && (r.functions == 0 || r.todos > 0) {
+        std::process::exit(1);
+    }
 }
-
-fn inspect(source: &str) {
-    let lines = source.lines().count();
-    let functions = source.matches("fn ").count();
-    let tests = source.matches("#[test]").count();
-    let unsafe_blocks = source.matches("unsafe").count();
-    let unwraps = source.matches("unwrap").count();
-    let todos = source.matches("TODO").count();
-    println!("Is this Rust yet? tiny source-text inspection\n");
-    println!("bytes: {}\nlines: {lines}\nfunctions: {functions}\ntests: {tests}\nunsafe mentions: {unsafe_blocks}\nunwrap mentions: {unwraps}\nTODOs: {todos}", source.len());
-    println!("\nchecklist:");
-    println!("  {} fn keyword(s): the compiler may recognize a function", if functions > 0 { "[x]" } else { "[ ]" });
-    println!("  {} test attribute(s): future-you has backup", if tests > 0 { "[x]" } else { "[ ]" });
-    println!("  {} TODO(s): the 2020 lockdown backlog lives", if todos > 0 { "[x]" } else { "[ ]" });
-    println!("\nverdict: {}", if functions > 0 { "Rust-shaped. Please ask rustc before ordering a commemorative hoodie." } else { "Rust-adjacent. It may be a README wearing a trench coat." });
+fn print_help() {
+    println!(
+        "{}\nCounts text patterns only; it is not rustc or static analysis.",
+        usage()
+    );
+}
+fn usage() -> &'static str {
+    "usage: is-this-rust-yet [--json] [--check] [FILE|-]"
+}
+fn parse_args(args: &[String]) -> Result<(bool, bool, Option<String>), String> {
+    let mut json = false;
+    let mut check = false;
+    let mut input = None;
+    for arg in args {
+        match arg.as_str() {
+            "--help" => {
+                print_help();
+                std::process::exit(0);
+            }
+            "--json" => json = true,
+            "--check" => check = true,
+            "-" => {
+                if input.is_some() {
+                    return Err("more than one input: -".into());
+                }
+                input = Some("-".into());
+            }
+            value if value.starts_with('-') => return Err(format!("unknown option {value}")),
+            value if input.is_some() => return Err(format!("more than one input: {value}")),
+            value => input = Some(value.to_string()),
+        }
+    }
+    Ok((json, check, input))
+}
+fn mark(ok: bool) -> &'static str {
+    if ok {
+        "[x]"
+    } else {
+        "[ ]"
+    }
+}
+fn inspect(source: &str) -> Report {
+    Report {
+        bytes: source.len(),
+        lines: source.lines().count(),
+        functions: source.matches("fn ").count(),
+        tests: source.matches("#[test]").count(),
+        unsafe_mentions: source.matches("unsafe").count(),
+        unwraps: source.matches("unwrap").count(),
+        todos: source.matches("TODO").count(),
+    }
+}
+fn report(r: &Report) {
+    println!("Is this Rust yet? tiny source-text inspection\n\nbytes: {}\nlines: {}\nfunctions: {}\ntests: {}\nunsafe mentions: {}\nunwrap mentions: {}\nTODOs: {}\n\nchecklist:\n  {} fn keyword(s): the compiler may recognize a function\n  {} test attribute(s): future-you has backup\n  {} TODO(s): the 2020 lockdown backlog lives\n  {} no unsafe mention: borrow checker vibes are pending\n\nverdict: {}", r.bytes, r.lines, r.functions, r.tests, r.unsafe_mentions, r.unwraps, r.todos, mark(r.functions > 0), mark(r.tests > 0), mark(r.todos > 0), mark(r.unsafe_mentions == 0), if r.functions > 0 { "Rust-shaped. Please ask rustc before ordering a commemorative hoodie." } else { "Rust-adjacent. It may be a README wearing a trench coat." });
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn empty_input_is_safe() {
+        assert_eq!(
+            inspect(""),
+            Report {
+                bytes: 0,
+                lines: 0,
+                functions: 0,
+                tests: 0,
+                unsafe_mentions: 0,
+                unwraps: 0,
+                todos: 0
+            }
+        );
+    }
+    #[test]
+    fn unicode_counts_bytes() {
+        assert_eq!(inspect("fn main() { println!(\"🦀\"); }").bytes, 31);
+    }
+    #[test]
+    fn flags_can_be_combined_and_reordered() {
+        let args = vec!["file.rs".into(), "--check".into(), "--json".into()];
+        assert_eq!(
+            parse_args(&args).unwrap(),
+            (true, true, Some("file.rs".into()))
+        );
+    }
+    #[test]
+    fn bad_cli_is_rejected() {
+        assert!(parse_args(&["--wat".into()]).is_err());
+        assert!(parse_args(&["a.rs".into(), "b.rs".into()]).is_err());
+    }
 }
